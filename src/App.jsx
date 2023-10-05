129d2a3057f6893a34826191188b15c367defd19
Vi Nguyen <khanhvi2941@gmail.com>
2023-10-05 17:08:52 +0700

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import {
   Avatar,
   Button,
+  Checkbox,
   Chip,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
   Menu,
   MenuItem,
+  OutlinedInput,
   Popover,
   Table,
   TableBody,
   TableContainer,
   TableHead,
   TableRow,
+  TextField,
 } from "@mui/material";
+import Input from "@mui/material/Input";
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
@@ -67,9 +75,16 @@ function App() {
     createData("Cupcake", 305, 3.7, 67, 4.3),
     createData("Gingerbread", 356, 16.0, 49, 3.9),
   ];
+
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
   return (
     <div className="flex flex-col h-screen bg-slate-100">
-      <div className=" h-[70px] sticky top-0 bg-[#313a46]">
+      {/* <div className=" h-[70px] sticky top-0 bg-[#313a46]">
         <div className="w-[1289px] m-auto flex justify-end h-full">
           <Button
             aria-describedby={id}
@@ -108,9 +123,9 @@ function App() {
             </Menu>
           </Popover>
         </div>
-      </div>
+      </div> */}
       <div className="flex-1 overflow-scroll">
-        <div className="bg-white h-14 drop-shadow-md ">
+        {/* <div className="bg-white h-14 drop-shadow-md ">
           <div className="w-[1289px] m-auto">
             <ul className="flex h-full hover:cursor-pointer">
               <li
@@ -123,9 +138,113 @@ function App() {
               <li className="px-3 py-4  hover:bg-slate-300">Bảng xếp hạng</li>
             </ul>
           </div>
-        </div>
+        </div> */}
         <div className="w-[1289px] m-auto  h-[1000px]  py-5">
-          <div>
+          {/* đăng nhập */}
+          <div className="my-[80px] flex rounded-[20px] overflow-hidden">
+            <div className="bg-[#ffca18] flex-1 min-h-[520px]"></div>
+            <div className="min-w-[400px] min-h-[520px] border m-auto px-6 py-8 bg-white">
+              <div className="flex flex-col  gap-5 items-stretch">
+                <div className="flex flex-col items-center">
+                  <p className="font-medium text-2xl mb-3">Welcome to VV</p>
+                  <p className="font-medium text-zinc-500 text-base">
+                    Log in Your Account
+                  </p>
+                </div>
+
+                <TextField
+                  label="Email"
+                  id="outlined-size-small"
+                  size="small"
+                  variant="standard"
+                  className="bg-white"
+                />
+
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                  <FormControl
+                    variant="standard"
+                    size="small"
+                    className="!bg-white pl-1"
+                  >
+                    <InputLabel htmlFor="standard-adornment-password">
+                      Password
+                    </InputLabel>
+                    <Input
+                      id="standard-adornment-password"
+                      type={showPassword ? "text" : "password"}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                          >
+                            {showPassword ? (
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                id="eye-off"
+                                width="18"
+                                height="18"
+                              >
+                                <g data-name="Layer 2">
+                                  <g data-name="eye-off">
+                                    <rect
+                                      width="24"
+                                      height="24"
+                                      opacity="0"
+                                    ></rect>
+                                    <path d="M4.71 3.29a1 1 0 0 0-1.42 1.42l5.63 5.63a3.5 3.5 0 0 0 4.74 4.74l5.63 5.63a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM12 13.5a1.5 1.5 0 0 1-1.5-1.5v-.07l1.56 1.56z"></path>
+                                    <path d="M12.22 17c-4.3.1-7.12-3.59-8-5a13.7 13.7 0 0 1 2.24-2.72L5 7.87a15.89 15.89 0 0 0-2.87 3.63 1 1 0 0 0 0 1c.63 1.09 4 6.5 9.89 6.5h.25a9.48 9.48 0 0 0 3.23-.67l-1.58-1.58a7.74 7.74 0 0 1-1.7.25zM21.87 11.5c-.64-1.11-4.17-6.68-10.14-6.5a9.48 9.48 0 0 0-3.23.67l1.58 1.58a7.74 7.74 0 0 1 1.7-.25c4.29-.11 7.11 3.59 8 5a13.7 13.7 0 0 1-2.29 2.72L19 16.13a15.89 15.89 0 0 0 2.91-3.63 1 1 0 0 0-.04-1z"></path>
+                                  </g>
+                                </g>
+                              </svg>
+                            ) : (
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                id="eye"
+                                width="18"
+                                height="18"
+                              >
+                                <g data-name="Layer 2">
+                                  <g data-name="eye">
+                                    <rect
+                                      width="24"
+                                      height="24"
+                                      opacity="0"
+                                    ></rect>
+                                    <path d="M21.87 11.5c-.64-1.11-4.16-6.68-10.14-6.5-5.53.14-8.73 5-9.6 6.5a1 1 0 0 0 0 1c.63 1.09 4 6.5 9.89 6.5h.25c5.53-.14 8.74-5 9.6-6.5a1 1 0 0 0 0-1zM12.22 17c-4.31.1-7.12-3.59-8-5 1-1.61 3.61-4.9 7.61-5 4.29-.11 7.11 3.59 8 5-1.03 1.61-3.61 4.9-7.61 5z"></path>
+                                    <path d="M12 8.5a3.5 3.5 0 1 0 3.5 3.5A3.5 3.5 0 0 0 12 8.5zm0 5a1.5 1.5 0 1 1 1.5-1.5 1.5 1.5 0 0 1-1.5 1.5z"></path>
+                                  </g>
+                                </g>
+                              </svg>
+                            )}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                    />
+                  </FormControl>
+                </div>
+                <div className="flex justify-between items-center">
+                  <div className="flex items-center">
+                    <Checkbox {...label} size="small" />
+                    <p className="ml-[2px] text-xs">Remember me</p>
+                  </div>
+                  <p className=" text-xs hover:underline">Forget password?</p>
+                </div>
+
+                <Button
+                  variant="contained"
+                  className="w-[200px] self-center !bg-[#ffca18] hover:bg-[#5d85d4]"
+                >
+                  Sign in
+                </Button>
+              </div>
+            </div>
+          </div>
+          {/* xếp hạng */}
+          {/* <div>
             <p className="font-semibold text-xl">Cuộc thi</p>
           </div>
           <div className="bg-[#f9bc0d] flex p-5">
@@ -194,7 +313,7 @@ function App() {
                 ))}
               </TableBody>
             </Table>
-          </TableContainer>
+          </TableContainer> */}
           {/* base ex */}
           {/* <div className="bg-white w-[594px] h-[380px] p-6 flex flex-col gap-8">
             <div className="flex items-center justify-between">
