6ae780c306cca5d2ace579817d0643cd51b233b1
Vi Nguyen <khanhvi2941@gmail.com>
2023-10-05 15:30:14 +0700

    ui competition result

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
-import { Avatar, Button, Chip, Menu, MenuItem, Popover } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Chip,
+  Menu,
+  MenuItem,
+  Popover,
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
 import { useState } from "react";
 
 const cuocThi = {
@@ -20,6 +35,38 @@ function App() {
 
   const open = Boolean(anchorEl);
   const id = open ? "simple-popover" : undefined;
+
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
+
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+  const rows = [
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+  ];
   return (
     <div className="flex flex-col h-screen bg-slate-100">
       <div className=" h-[70px] sticky top-0 bg-[#313a46]">
@@ -78,7 +125,78 @@ function App() {
           </div>
         </div>
         <div className="w-[1289px] m-auto  h-[1000px]  py-5">
-          <div className="bg-white w-[594px] h-[380px] p-6 flex flex-col gap-8">
+          <div>
+            <p className="font-semibold text-xl">Cuộc thi</p>
+          </div>
+          <div className="bg-[#f9bc0d] flex p-5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              data-name="Layer 1"
+              viewBox="0 0 24 24"
+              id="Championtrophy"
+              width={100}
+              className="px-3"
+            >
+              <path
+                d="M18 4V2H6v2a4 4 0 0 0 0 8h.38a6.18 6.18 0 0 0 1.41 2.21A4.07 4.07 0 0 1 9 17H6v3H5v2h14v-2h-1v-3h-3a4.07 4.07 0 0 1 1.2-2.79A6.18 6.18 0 0 0 17.62 12H18a4 4 0 0 0 0-8ZM6 10a2 2 0 0 1 0-4Zm4-4h4v2h-4Zm6 13v1H8v-1h8Zm2-9V6a2 2 0 0 1 0 4Z"
+                fill="#ffffff"
+                className="color000000 svgShape"
+              ></path>
+            </svg>
+            <div className="text-white ">
+              <div>
+                <p className="font-medium text-lg">{cuocThi.name}</p>
+                <p className="text-sm opacity-50">36 người tham gia</p>
+              </div>
+              <div className="flex gap-6 mt-4">
+                <div>
+                  <p>{cuocThi.time}</p>
+                  <p className="text-sm opacity-50">Ngày bắt đầu</p>
+                </div>
+                <div>
+                  {cuocThi.status === 0 && <p>Sắp diễn ra</p>}
+                  {cuocThi.status === 1 && <p>Đang diễn ra</p>}
+                  {cuocThi.status === 2 && <p>Kết thúc</p>}
+                  <p className="text-sm opacity-50">Trạng thái</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>Thứ hạng</StyledTableCell>
+                  <StyledTableCell align="right">Mã sinh viên</StyledTableCell>
+                  <StyledTableCell align="right">Họ tên</StyledTableCell>
+                  <StyledTableCell align="right">Điểm</StyledTableCell>
+
+                  <StyledTableCell align="right">
+                    Protein&nbsp;(g)
+                  </StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <StyledTableRow key={row.name}>
+                    <StyledTableCell component="th" scope="row">
+                      {row.name}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {row.calories}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                    <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                    <StyledTableCell align="right">
+                      {row.protein}
+                    </StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          {/* base ex */}
+          {/* <div className="bg-white w-[594px] h-[380px] p-6 flex flex-col gap-8">
             <div className="flex items-center justify-between">
               <div>
                 <p className="font-semibold mb-3">{cuocThi.name}</p>
@@ -185,7 +303,7 @@ function App() {
             <p className="text-xs uppercase font-medium ">Thời gian</p>
             <p>{cuocThi.time}</p>
             <p>{cuocThi.address}</p>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
