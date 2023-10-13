0b876088aaad84a757af9c57171629b77a2056c9
Vi Nguyen <khanhvi2941@gmail.com>
2023-10-13 16:46:23 +0700

    custom header table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,26 +1,55 @@
-import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarExport,
+  GridToolbarFilterButton,
+} from "@mui/x-data-grid";
 
 import { useState } from "react";
 
 const VISIBLE_FIELDS = ["name", "rating", "country", "dateCreated", "isAdmin"];
 
-const Table = () => {
+function CustomToolbar() {
+  return (
+    <GridToolbarContainer>
+      <GridToolbarFilterButton />
+      <GridToolbarExport />
+    </GridToolbarContainer>
+  );
+}
+
+const Table = ({ rows, columnss }) => {
   const [tableData, setTableData] = useState([
     { id: "1", title: "hh", body: "hhihih" },
   ]);
   const columns = [
-    { field: "id", headerName: "ID" },
+    {
+      field: "id",
+      headerName: "ID",
+    },
     { field: "title", headerName: "Title", width: 300 },
     { field: "body", headerName: "Body", width: 600 },
   ];
   return (
-    <div style={{ height: 400, width: "100%" }}>
-      <DataGrid
-        rows={tableData}
-        columns={columns}
-        pageSize={12}
-        slots={{ toolbar: GridToolbar }}
-      />
+    <div className="flex justify-center w-full">
+      <div className="w-[1350px] bg-white mt-7">
+        <div style={{ height: 614, width: "100%" }}>
+          <DataGrid
+            rows={tableData}
+            columns={columns}
+            pageSize={12}
+            slots={{ toolbar: CustomToolbar }}
+            sx={{
+              ".MuiDataGrid-columnHeaderTitle": {
+                fontWeight: "600 !important",
+                overflow: "visible !important",
+                textTransform: "uppercase",
+                color: "#616060",
+              },
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
