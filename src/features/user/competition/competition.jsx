a538713a477f4a72ae4d4343883b03d69cd04512
Vi Nguyen <khanhvi2941@gmail.com>
2023-11-12 21:53:18 +0700

    competition detail update

--- src/features/user/competition/competition.jsx
+++ src/features/user/competition/competition.jsx
@@ -10,10 +10,12 @@ import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import { styled } from "@mui/material/styles";
 
 const cuocThi = {
-  name: "Lập trình web",
+  name: "Kiểm thử phần mềm",
   status: 2,
-  time: "20-10-2021",
-  address: "B201",
+  timeStart: "01-11-2023",
+  timeEnd: "11-11-2023",
+  soVong: 2,
+  people: 12,
 };
 const Competition = () => {
   const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -27,15 +29,18 @@ const Competition = () => {
   }));
 
   const rows = [
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-    createData("Eclair", 262, 16.0, 24, 6.0),
-    createData("Cupcake", 305, 3.7, 67, 4.3),
-    createData("Gingerbread", 356, 16.0, 49, 3.9),
+    createData(1, 1, "Phạm Thiên An", 9.0),
+    createData(2, 6, "Trần Ngọc Bảo", 8.3),
+    createData(3, 3, "Trần Đình Hưng", 8.0),
+    createData(4, 5, "Võ Tiến Khoa", 7.3),
+    createData(5, 7, "Võ Thị Bích Loan", 6.9),
+    createData(6, 2, "Nguyễn Lê Minh", 6.6),
+    createData(7, 8, "Nguyễn Thị Thùy Linh", 6.59),
+    createData(8, 9, "Vũ Trung Kiên", 5.9),
   ];
 
-  function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
+  function createData(name, calories, fat, carbs) {
+    return { name, calories, fat, carbs };
   }
 
   const StyledTableRow = styled(TableRow)(({ theme }) => ({
@@ -49,9 +54,8 @@ const Competition = () => {
   }));
   return (
     <div>
-      {" "}
       <div>
-        <p className="font-semibold text-xl">Cuộc thi</p>
+        <p className="font-semibold text-xl mb-3">Cuộc thi</p>
       </div>
       <div className="bg-[#f9bc0d] flex p-5">
         <svg
@@ -71,32 +75,38 @@ const Competition = () => {
         <div className="text-white ">
           <div>
             <p className="font-medium text-lg">{cuocThi.name}</p>
-            <p className="text-sm opacity-50">36 người tham gia</p>
+            <p className="text-sm opacity-60">{cuocThi.people} participants</p>
           </div>
           <div className="flex gap-6 mt-4">
             <div>
-              <p>{cuocThi.time}</p>
-              <p className="text-sm opacity-50">Ngày bắt đầu</p>
+              <p>{cuocThi.timeStart}</p>
+              <p className="text-sm opacity-60">Start Time</p>
             </div>
             <div>
-              {cuocThi.status === 0 && <p>Sắp diễn ra</p>}
-              {cuocThi.status === 1 && <p>Đang diễn ra</p>}
-              {cuocThi.status === 2 && <p>Kết thúc</p>}
-              <p className="text-sm opacity-50">Trạng thái</p>
+              {cuocThi.status === 0 && <p>Upcoming</p>}
+              {cuocThi.status === 1 && <p>In progress</p>}
+              {cuocThi.status === 2 && <p>Completed</p>}
+              {cuocThi.status === 3 && <p>Canceled</p>}
+              <p className="text-sm opacity-60">Status</p>
             </div>
           </div>
         </div>
+      </div>
+      <div className="bg-white w-full px-5 flex flex-col gap-1 py-3 text-[#838c96]">
+        <p>Number of prizes: 3</p>
+        <p>Maximum number of participants: 20</p>
+        <p> Minimum number of participants: 4</p>
       </div>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 700 }} aria-label="customized table">
           <TableHead>
             <TableRow>
-              <StyledTableCell>Thứ hạng</StyledTableCell>
-              <StyledTableCell align="right">Mã sinh viên</StyledTableCell>
-              <StyledTableCell align="right">Họ tên</StyledTableCell>
-              <StyledTableCell align="right">Điểm</StyledTableCell>
+              <StyledTableCell>Rank</StyledTableCell>
+              <StyledTableCell align="right"> Student ID</StyledTableCell>
+              <StyledTableCell align="right"> Full name</StyledTableCell>
+              <StyledTableCell align="right"> Score</StyledTableCell>
 
-              <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+              <StyledTableCell align="right"></StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
--- src/features/user/competitionListFetch/CompetitionListFetch.jsx
+++ src/features/user/competitionListFetch/CompetitionListFetch.jsx
@@ -1,135 +1,176 @@
 import { Button, Chip } from "@mui/material";
+import { Link } from "react-router-dom";
 
-const cuocThi = {
-  name: "Lập trình web",
-  status: 2,
-  timeStart: "20-10-2021",
-  timeEnd: "20-10-20",
-  soVong: 3,
-};
+const competitions = [
+  {
+    name: "Bảo mật thông tin",
+    status: 0,
+    timeStart: "19-11-2023",
+    timeEnd: "22-11-2023",
+    soVong: 2,
+    people: 5,
+  },
+  {
+    name: "Trí tuệ nhân tạo",
+    status: 0,
+    timeStart: "19-11-2023",
+    timeEnd: "22-11-2023",
+    soVong: 2,
+    people: 5,
+  },
+  {
+    name: "Lập trình ứng dụng",
+    status: 1,
+    timeStart: "10-11-2023",
+    timeEnd: "05-12-2023",
+    soVong: 3,
+    people: 2,
+  },
+  {
+    name: "Kiểm thử phần mềm",
+    status: 2,
+    timeStart: "01-11-2023",
+    timeEnd: "11-11-2023",
+    soVong: 2,
+    people: 12,
+  },
+
+  {
+    name: "Lập trình Java",
+    status: 3,
+    timeStart: "10-11-2023",
+    timeEnd: "10-11-2023",
+    soVong: 2,
+    people: 5,
+  },
+];
 const CompetitionListFetch = () => {
   return (
-    <>
-      {" "}
-      <div className="bg-white w-[594px] h-[380px] p-5 flex flex-col gap-8 drop-shadow-md rounded-lg">
-        <div className="flex items-center justify-between">
-          <div>
-            <p className="font-semibold mb-3">{cuocThi.name}</p>
-            {cuocThi.status === 0 && (
-              <Chip
-                label="Upcoming"
-                className="!bg-[#ddf3f9] !text-[#38c0e6] !font-medium"
-              />
-            )}
-            {cuocThi.status === 1 && (
-              <Chip
-                label=" In progress"
-                className="!bg-[#ddf7ed] !text-[#28f2a5] !font-medium"
-              />
-            )}
-            {cuocThi.status === 2 && (
-              <Chip
-                label="Completed"
-                className="!bg-[#e8fbbb] !text-[#c3ed4f] !font-medium"
-              />
-            )}
-            {cuocThi.status === 3 && (
-              <Chip
-                label="Canceled"
-                className="!bg-[#f6b2a6] !text-[#f54323] !font-medium"
-              />
-            )}
-          </div>
-          {cuocThi.status === 0 && (
-            <Button className="!bg-[#44badc] !text-white">Registration</Button>
-          )}
-          {cuocThi.status === 2 && (
-            <Button className="!bg-[#ec4848] !text-white">Results</Button>
-          )}
-        </div>
-
-        <p className="text-gray-500 flex gap-1 items-center hover:underline">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            id="List"
-            width={24}
-          >
-            <g
-              data-name="Layer 2"
-              fill="#a4a7a8"
-              className="color000000 svgShape"
-            >
-              <g
-                data-name="list"
-                fill="#a4a7a8"
-                className="color000000 svgShape"
+    <div className="flex flex-wrap gap-5">
+      {competitions.map((cuocThi, index) => (
+        <Link key={index} to={"/detail"}>
+          <div className="bg-white w-[416px] h-[330px] p-5 flex flex-col gap-8 drop-shadow-md rounded-lg">
+            <div className="flex items-center justify-between">
+              <div>
+                <p className="font-semibold mb-3">{cuocThi.name}</p>
+                {cuocThi.status === 0 && (
+                  <Chip
+                    label="Upcoming"
+                    className="!bg-[#ddf3f9] !text-[#38c0e6] !font-medium"
+                  />
+                )}
+                {cuocThi.status === 1 && (
+                  <Chip
+                    label=" In progress"
+                    className="!bg-[#ddf7ed] !text-[#28f2a5] !font-medium"
+                  />
+                )}
+                {cuocThi.status === 2 && (
+                  <Chip
+                    label="Completed"
+                    className="!bg-[#e8fbbb] !text-[#c3ed4f] !font-medium"
+                  />
+                )}
+                {cuocThi.status === 3 && (
+                  <Chip
+                    label="Canceled"
+                    className="!bg-[#f6b2a6] !text-[#f54323] !font-medium"
+                  />
+                )}
+              </div>
+              {cuocThi.status === 0 && (
+                <Button className="!bg-[#44badc] !text-white">
+                  Registration
+                </Button>
+              )}
+              {cuocThi.status === 2 && (
+                <Button className="!bg-[#ec4848] !text-white">Results</Button>
+              )}
+            </div>
+            <p className="text-gray-500 flex gap-1 items-center hover:underline">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                id="List"
+                width={24}
               >
-                <circle
-                  cx="4"
-                  cy="7"
-                  r="1"
+                <g
+                  data-name="Layer 2"
                   fill="#a4a7a8"
                   className="color000000 svgShape"
-                ></circle>
-                <circle
-                  cx="4"
-                  cy="12"
-                  r="1"
-                  fill="#a4a7a8"
-                  className="color000000 svgShape"
-                ></circle>
-                <circle
-                  cx="4"
-                  cy="17"
-                  r="1"
-                  fill="#a4a7a8"
-                  className="color000000 svgShape"
-                ></circle>
-                <rect
-                  width="14"
-                  height="2"
-                  x="7"
-                  y="11"
-                  rx=".94"
-                  ry=".94"
-                  fill="#a4a7a8"
-                  className="color000000 svgShape"
-                ></rect>
-                <rect
-                  width="14"
-                  height="2"
-                  x="7"
-                  y="16"
-                  rx=".94"
-                  ry=".94"
-                  fill="#a4a7a8"
-                  className="color000000 svgShape"
-                ></rect>
-                <rect
-                  width="14"
-                  height="2"
-                  x="7"
-                  y="6"
-                  rx=".94"
-                  ry=".94"
-                  fill="#a4a7a8"
-                  className="color000000 svgShape"
-                ></rect>
-              </g>
-            </g>
-          </svg>
-          <span>36 participants</span>
-        </p>
-        <p className="text-xs uppercase font-medium ">DURATION</p>
-        <div className="flex w-[80%] justify-between">
-          <p>{cuocThi.timeStart}</p>
-          <p>{cuocThi.timeEnd}</p>
-        </div>
+                >
+                  <g
+                    data-name="list"
+                    fill="#a4a7a8"
+                    className="color000000 svgShape"
+                  >
+                    <circle
+                      cx="4"
+                      cy="7"
+                      r="1"
+                      fill="#a4a7a8"
+                      className="color000000 svgShape"
+                    ></circle>
+                    <circle
+                      cx="4"
+                      cy="12"
+                      r="1"
+                      fill="#a4a7a8"
+                      className="color000000 svgShape"
+                    ></circle>
+                    <circle
+                      cx="4"
+                      cy="17"
+                      r="1"
+                      fill="#a4a7a8"
+                      className="color000000 svgShape"
+                    ></circle>
+                    <rect
+                      width="14"
+                      height="2"
+                      x="7"
+                      y="11"
+                      rx=".94"
+                      ry=".94"
+                      fill="#a4a7a8"
+                      className="color000000 svgShape"
+                    ></rect>
+                    <rect
+                      width="14"
+                      height="2"
+                      x="7"
+                      y="16"
+                      rx=".94"
+                      ry=".94"
+                      fill="#a4a7a8"
+                      className="color000000 svgShape"
+                    ></rect>
+                    <rect
+                      width="14"
+                      height="2"
+                      x="7"
+                      y="6"
+                      rx=".94"
+                      ry=".94"
+                      fill="#a4a7a8"
+                      className="color000000 svgShape"
+                    ></rect>
+                  </g>
+                </g>
+              </svg>
+              <span>{cuocThi.people} participants</span>
+            </p>
+            <p className="text-xs uppercase font-medium ">DURATION</p>
+            <div className="flex w-[80%] justify-between">
+              <p>{cuocThi.timeStart}</p>
+              <p>{cuocThi.timeEnd}</p>
+            </div>
 
-        <p>{cuocThi.soVong} rounds</p>
-      </div>
-    </>
+            <p>{cuocThi.soVong} rounds</p>
+          </div>
+        </Link>
+      ))}
+    </div>
   );
 };
 
