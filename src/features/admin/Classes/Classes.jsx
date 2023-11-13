1f0f968a3952d6a2be9883a055df195dac2d4dbc
Vi Nguyen <khanhvi2941@gmail.com>
2023-11-13 22:51:18 +0700

    link active navbar

--- src/features/admin/Classes/Classes.jsx
+++ src/features/admin/Classes/Classes.jsx
@@ -1,5 +1,4 @@
-import { Button, Chip } from "@mui/material";
-import { GridActionsCellItem } from "@mui/x-data-grid";
+import { Button } from "@mui/material";
 import Table from "../../../components/Table/Table";
 
 const Classes = () => {
@@ -7,87 +6,28 @@ const Classes = () => {
     {
       field: "id",
       headerName: "ID",
-      width: 100,
+      width: 150,
     },
 
     { field: "courseid", headerName: "courseid", width: 200 },
-    { field: "name", headerName: "fullname", width: 250 },
-    {
-      field: "active",
-      headerName: "Active",
-      width: 200,
-      renderCell: (params) => {
-        return params.row.active ? (
-          <Chip
-            label="Active"
-            color="success"
-            variant="outlined"
-            className="w-20 !h-7"
-          />
-        ) : (
-          <Chip
-            label="Active"
-            color="success"
-            variant="outlined"
-            className="w-20 !h-7"
-          />
-        );
-      },
-    },
-    {
-      field: "actions",
-      type: "actions",
-      headerName: "Actions",
-      getActions: (params) => [
-        <GridActionsCellItem
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              id="Lock"
-              width={15}
-            >
-              <path
-                d="M12,13a1.49,1.49,0,0,0-1,2.61V17a1,1,0,0,0,2,0V15.61A1.49,1.49,0,0,0,12,13Zm5-4V7A5,5,0,0,0,7,7V9a3,3,0,0,0-3,3v7a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V12A3,3,0,0,0,17,9ZM9,7a3,3,0,0,1,6,0V9H9Zm9,12a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V12a1,1,0,0,1,1-1H17a1,1,0,0,1,1,1Z"
-                fill="#151515"
-                className="color000000 svgShape"
-              ></path>
-            </svg>
-          }
-          label="Block"
-        />,
-        <GridActionsCellItem
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              data-name="Layer 1"
-              viewBox="0 0 32 32"
-              id="notification"
-              width={30}
-            >
-              <path d="M26.59 21.17a2 2 0 0 1-.59-1.41V14a10 10 0 0 0-7.64-9.71 2.47 2.47 0 0 0 .14-.79 2.5 2.5 0 0 0-5 0 2.47 2.47 0 0 0 .14.79A10 10 0 0 0 6 14v5.76a2 2 0 0 1-.59 1.41A4.79 4.79 0 0 0 4 24.59V25a2 2 0 0 0 2 2h7.18a3 3 0 0 0-.18 1 3 3 0 0 0 6 0 3 3 0 0 0-.18-1H26a2 2 0 0 0 2-2v-.41a4.79 4.79 0 0 0-1.41-3.42ZM15.5 3.5a.5.5 0 1 1 .5.5.5.5 0 0 1-.5-.5Z"></path>
-            </svg>
-          }
-          label="Print"
-          showInMenu
-        />,
-      ],
-    },
+    { field: "name", headerName: "name", width: 250 },
+    { field: "timestart", headerName: "timestart", width: 250 },
+    { field: "timeend", headerName: "timeend", width: 250 },
   ];
   const rows = [
     {
       id: "1",
-      accountid: "4",
+      courseid: "4",
       email: "sv01@gmail.com",
       active: 1,
-      fullname: "Nguyễn Thúy Hạnh",
+      name: "Nguyễn Thúy Hạnh",
     },
     {
       id: "2",
       email: "sv02@gmail.com",
-      accountid: "6",
+      courseid: "6",
       active: 1,
-      fullname: "Trần Thiên Bảo",
+      name: "Trần Thiên Bảo",
     },
   ];
   return (
--- src/layouts/admin/components/Navbar.jsx
+++ src/layouts/admin/components/Navbar.jsx
@@ -1,13 +1,65 @@
-import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import { Button } from "@mui/material";
+import { NavLink, matchPath, useLocation } from "react-router-dom";
 import logo from "../../../assets/logo.png";
 
 const Navbar = () => {
+  const { pathname } = useLocation();
+
+  const navbarItems = [
+    {
+      title: "Home",
+      icon: <i className="fa-light fa-house"></i>,
+      iconActive: <i className="fa-solid fa-house"></i>,
+      path: "/admin/classes",
+    },
+
+    {
+      title: "Bookmark",
+      icon: <i className="fa-light fa-bookmark"></i>,
+      iconActive: <i className="fa-solid fa-bookmark"></i>,
+      path: "/classes",
+    },
+    // {
+    //   title: "Stories",
+    //   icon: <i className="fa-light fa-rectangle-history"></i>,
+    //   iconActive: <i className="fa-solid fa-rectangle-history"></i>,
+    //   path: appRoutes.STORIES,
+    // },
+    // {
+    //   title: "write",
+    //   icon: <i className="fa-light fa-file-pen"></i>,
+    //   iconActive: <i className="fa-solid fa-file-pen"></i>,
+    //   path: appRoutes.WRITE,
+    // },
+  ];
   return (
-    <div className="w-[280px] h-full bg-[#000] text-white">
+    <div className="w-[280px] h-full bg-[#000] text-white ">
       <div>
         <img src={logo} alt="logo" className="" />
       </div>
-      <List>
+      <div className="flex w-full flex-col  text-center mt-1">
+        {navbarItems.map((item) => {
+          const isActive = matchPath(item.path, pathname);
+
+          return (
+            <NavLink
+              key={item.title}
+              to={item.path}
+              className="active text-white"
+            >
+              <Button
+                className={`!text-white rounded-md hover:!bg-slate-500 w-[96%] ${
+                  isActive ? "!bg-slate-500" : ""
+                }`}
+                size="medium"
+              >
+                {item.title}
+              </Button>
+            </NavLink>
+          );
+        })}
+      </div>
+      {/* <List>
         {[
           "Home",
           "Competition",
@@ -19,13 +71,13 @@ const Navbar = () => {
           "Register",
           "Rounds",
         ].map((text, index) => (
-          <ListItem key={text} disablePadding className="m-auto !w-[94%]">
+          <ListItem key={index} disablePadding className="m-auto !w-[94%]">
             <ListItemButton className=" hover:!bg-[#404552] !rounded-md ">
               <ListItemText primary={text} className=" font-semibold" />
             </ListItemButton>
           </ListItem>
         ))}
-      </List>
+      </List> */}
     </div>
   );
 };
