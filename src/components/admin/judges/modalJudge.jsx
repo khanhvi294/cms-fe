0ad8b645aed8989245d5527088c0a43da848de2b
Vi Nguyen <khanhvi2941@gmail.com>
2023-11-19 18:15:46 +0700

    display round in judge

--- src/components/admin/judges/modalJudge.jsx
+++ src/components/admin/judges/modalJudge.jsx
@@ -136,6 +136,100 @@ const ModalJudge = ({ open, setOpenJudge, round }) => {
               </svg>
             </IconButton>
           </div>
+          <div>
+            <div className="bg-white min-h-[200px]  rounded-2xl  p-4 gap-5 ">
+              <Typography
+                id="modal-modal-title"
+                variant="h6"
+                component="h2"
+                className="font-bold self-center"
+              >
+                Round
+              </Typography>
+              <div className=" flex justify-between">
+                <div className="">
+                  <div className="flex justify-between w-full">
+                    <p className="font-bold">Id</p>
+                    <p>{round?.id}</p>
+                  </div>
+
+                  <div className="flex justify-between w-full">
+                    <p className="font-bold">Name</p>
+                    <p>{round?.name}</p>
+                  </div>
+                  <div className="flex justify-between w-full">
+                    <p className="font-bold">EmpoyeeId</p>
+                    <p>{round?.employeeId}</p>
+                  </div>
+                  <div className="flex justify-between w-full">
+                    <p className="font-bold">Number of prizes</p>
+                    <p>{round?.numOfPrizes}</p>
+                  </div>
+                </div>
+                <div>
+                  <div className="flex justify-between w-full">
+                    <p className="font-bold">Number min</p>
+                    <p>{round?.minimumQuantity}</p>
+                  </div>
+                  <div className="flex justify-between w-full">
+                    <p className="font-bold">Number max</p>
+                    <p>{round?.maximumQuantity}</p>
+                  </div>
+                  <div className="flex justify-between w-full">
+                    <p className="font-bold">Rounds</p>
+                    <p>{round?.numberOfRound}</p>
+                  </div>
+                </div>
+              </div>
+              {/* <ModalSeeRound competition={competition} /> */}
+            </div>
+
+            <div>
+              <div className="flex gap-2 justify-between items-center">
+                <span className="text-2xl font-semibold">Judges</span>
+                <Button
+                  variant="contained flex-end !bg-[#000] !text-white !rounded-md"
+                  //   onClick={handleOpen}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    id="plus"
+                    width={22}
+                    height={22}
+                  >
+                    <g
+                      data-name="Layer 2"
+                      fill="#ffffff"
+                      className="color000000 svgShape"
+                    >
+                      <g
+                        data-name="plus"
+                        fill="#ffffff"
+                        className="color000000 svgShape"
+                      >
+                        <rect
+                          width="24"
+                          height="24"
+                          opacity="0"
+                          transform="rotate(180 12 12)"
+                          fill="#ffffff"
+                          className="color000000 svgShape"
+                        ></rect>
+                        <path
+                          d="M19 11h-6V5a1 1 0 0 0-2 0v6H5a1 1 0 0 0 0 2h6v6a1 1 0 0 0 2 0v-6h6a1 1 0 0 0 0-2z"
+                          fill="#ffffff"
+                          className="color000000 svgShape"
+                        ></path>
+                      </g>
+                    </g>
+                  </svg>
+                  Add
+                </Button>
+              </div>
+              <Table columns={columns} rows={rows} />
+            </div>
+          </div>
           <div className="flex gap-2 justify-between items-center">
             <span className="text-2xl font-semibold">Rounds</span>
             <Button
