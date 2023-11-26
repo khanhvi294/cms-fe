8adf30bc46a87f25c711c502dbc7241b828f1b89
Vi Nguyen <khanhvi2941@gmail.com>
2023-11-27 01:15:18 +0700

    get class join competition

--- src/features/admin/Competitions/CompetitionDetail.jsx
+++ src/features/admin/Competitions/CompetitionDetail.jsx
@@ -14,6 +14,7 @@ import {
 } from "../../../services/competitionService";
 import { createExamForm } from "../../../services/examFormService";
 import { getRoundByCompetition } from "../../../services/roundService";
+import Table from "../../../components/Table/Table";
 
 const CompetitionDetail = () => {
   const { id } = useParams();
@@ -162,6 +163,70 @@ const CompetitionDetail = () => {
     },
   });
 
+  // class
+
+  const columnsClass = [
+    {
+      field: "id",
+      headerName: "ID",
+      width: 450,
+    },
+    { field: "name", headerName: "name", width: 450 },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Actions",
+      getActions: (params) => [
+        <GridActionsCellItem
+          icon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              id="Delete"
+              x="0"
+              y="0"
+              version="1.1"
+              viewBox="0 0 29 29"
+              xmlSpace="preserve"
+              width={15}
+              onClick={() => {
+                setClassDelete(params?.row);
+                setOpenDelete(true);
+              }}
+            >
+              <path
+                d="M19.795 27H9.205a2.99 2.99 0 0 1-2.985-2.702L4.505 7.099A.998.998 0 0 1 5.5 6h18a1 1 0 0 1 .995 1.099L22.78 24.297A2.991 2.991 0 0 1 19.795 27zM6.604 8 8.21 24.099a.998.998 0 0 0 .995.901h10.59a.998.998 0 0 0 .995-.901L22.396 8H6.604z"
+                fill="#151515"
+                className="color000000 svgShape"
+              ></path>
+              <path
+                d="M26 8H3a1 1 0 1 1 0-2h23a1 1 0 1 1 0 2zM14.5 23a1 1 0 0 1-1-1V11a1 1 0 1 1 2 0v11a1 1 0 0 1-1 1zM10.999 23a1 1 0 0 1-.995-.91l-1-11a1 1 0 0 1 .905-1.086 1.003 1.003 0 0 1 1.087.906l1 11a1 1 0 0 1-.997 1.09zM18.001 23a1 1 0 0 1-.997-1.09l1-11c.051-.55.531-.946 1.087-.906a1 1 0 0 1 .905 1.086l-1 11a1 1 0 0 1-.995.91z"
+                fill="#151515"
+                className="color000000 svgShape"
+              ></path>
+              <path
+                d="M19 8h-9a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h9a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1zm-8-2h7V4h-7v2z"
+                fill="#151515"
+                className="color000000 svgShape"
+              ></path>
+            </svg>
+          }
+          label="Delete"
+        />,
+      ],
+    },
+  ];
+  const [rowsClass, setRowsClass] = useState([]);
+
+  useQuery({
+    queryKey: ["classCompetition", id],
+    enabled: !!id,
+    queryFn: () => getAllClassJoinCompetition(id),
+    onSuccess: (data) => {
+      // console.log(data);
+      setRowsClass(data.data.data);
+    },
+  });
+
   return (
     <>
       <div>
@@ -229,7 +294,7 @@ const CompetitionDetail = () => {
         </div>
 
         <div>
-          <div className="flex gap-2 justify-between items-center mb-4">
+          {/* <div className="flex gap-2 justify-between items-center mb-4">
             <span className="text-xl font-semibold">Rounds</span>
             <Button
               variant="contained flex-end !bg-[#000] !text-white !rounded-md"
@@ -273,6 +338,11 @@ const CompetitionDetail = () => {
           </div>
           <div className="overflow-auto h-[400px]">
             <RoundTable rows={rows} />
+          </div> */}
+          {/* class */}
+          <p>Class</p>
+          <div>
+            <Table columns={columnsClass} rows={rowsClass} />
           </div>
         </div>
       </div>
