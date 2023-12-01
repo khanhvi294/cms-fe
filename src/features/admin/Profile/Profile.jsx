92ffafd9d69b5d794fbbd2cc3d126586c1e2d631
Vi Nguyen <khanhvi2941@gmail.com>
2023-12-01 16:03:31 +0700

    update profile ui

--- src/features/admin/Profile/Profile.jsx
+++ src/features/admin/Profile/Profile.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   Avatar,
   Box,
@@ -11,21 +10,25 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { useMutation } from "react-query";
-import { updateEmployee } from "../../../services/employeeService";
-import { setUser } from "../../../redux/slices/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import PhotoUpload from "../../../components/Profile/PhotoUpload";
+import { setUser } from "../../../redux/slices/userSlice";
+import { updateEmployee } from "../../../services/employeeService";
 import { uploadImg } from "../../../utils/cloundinaryFns";
+import { format } from "date-fns";
 
 // import { uploadImg } from "../../../../utils/firebaseFns";
 
 const BIO_MAX_LENGTH = 160;
 const Profile = () => {
-  const user = useSelector((state) => state.user.data.info);
+  const user = useSelector((state) => state.user.data.info.accountEmployee);
+  const today = new Date();
+  const formattedToday = format(today, "yyyy-MM-dd");
+  console.log(user);
   const dispatch = useDispatch();
   const updateProfileMutation = useMutation({
     mutationFn: updateEmployee,
@@ -63,121 +66,204 @@ const Profile = () => {
   };
 
   return (
-    <Box className="container absolute inset-1/2 flex h-[500px] w-[554px] min-w-fit  translate-x-[-50%] translate-y-[-50%] flex-col justify-around rounded border-none bg-white p-8 shadow-xl">
-      <Typography
-        id="modal-modal-title"
-        variant="h6"
-        component="h2"
-        className="mb-5 font-bold"
-      >
-        Profile information
-      </Typography>
-      <div>
+    <div className="flex gap-9">
+      <Box className="container flex h-[300px] w-[300px]   flex-col justify-around items-center rounded border bg-white p-8 shadow-xl">
+        <Typography
+          id="modal-modal-title"
+          variant="h6"
+          component="h2"
+          className="mb-5 font-bold"
+        >
+          Profile information
+        </Typography>
         <div>
-          <label className="text-[13px] text-slate-500">Photo</label>
-          <div className="flex min-w-min justify-between py-3.5">
-            <div>
-              <div className="flex items-center justify-center">
-                <PhotoUpload onUpload={setUploadData}>
-                  <Avatar
-                    sx={{ width: 70, height: 70 }}
-                    src={upLoadData?.previewImg}
-                  />
-                </PhotoUpload>
+          <div>
+            <label className="text-[13px] text-slate-500">Photo</label>
+            <div className=" min-w-min justify-between py-3.5">
+              <div>
+                <div className="flex items-center justify-center">
+                  <PhotoUpload onUpload={setUploadData}>
+                    <Avatar
+                      sx={{ width: 100, height: 100 }}
+                      src={upLoadData?.previewImg}
+                    />
+                  </PhotoUpload>
+                </div>
               </div>
-            </div>
-            <div className="px-2.5">
-              <Button
-                variant="text"
-                component="label"
-                className="min-w-min px-0 text-xs font-normal text-lime-600 hover:bg-white"
-              >
-                <p>Upload</p>
-                {/* <PhotoUpload onUpload={setUploadData}></PhotoUpload> */}
-              </Button>
+              <div className="px-2.5 ">
+                <Button
+                  variant="text"
+                  component="label"
+                  className="min-w-min px-0 text-xs font-normal text-lime-600 hover:bg-white"
+                >
+                  <p>Upload</p>
+                  {/* <PhotoUpload onUpload={setUploadData}></PhotoUpload> */}
+                </Button>
 
-              <Button
-                variant="text"
-                component="label"
-                className="ml-3 min-w-min px-0 text-xs font-normal text-red-600 hover:bg-white"
-                onClick={() => setUploadData("")}
-              >
-                Remove
-              </Button>
-              <Typography className="= text-xs text-gray-400">
-                Recommended: Square JPG, PNG, or GIF, at least 1,000 pixels per
-                side.
-              </Typography>
+                <Button
+                  variant="text"
+                  component="label"
+                  className="ml-3 min-w-min px-0 text-xs font-normal text-red-600 hover:bg-white"
+                  onClick={() => setUploadData("")}
+                >
+                  Remove
+                </Button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <TextField
-        error={!!errors?.username}
-        label="Name*"
-        variant="standard"
-        defaultValue={user.username}
-        helperText={errors?.username?.message}
-        {...register("username", {
-          required: "user name is required filed",
-          maxLength: 60,
-        })}
-      />
-
-      <TextField
-        id="standard-helperText"
-        label="Bio"
-        {...register("bio", {
-          maxLength: BIO_MAX_LENGTH,
-        })}
-        defaultValue={user?.bio}
-        variant="standard"
-      />
-
-      <FormControl>
-        <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
-        <RadioGroup
-          aria-labelledby="demo-radio-buttons-group-label"
-          defaultValue={user.gender}
-          name="radio-buttons-group"
-          row
-        >
-          <FormControlLabel
-            {...register("gender")}
-            value={true}
-            control={<Radio />}
-            label="Female"
+      </Box>
+      <div className="flex flex-col justify-around rounded border bg-white p-8 shadow-xl w-[900px] gap-6">
+        <div className="flex gap-5">
+          <TextField
+            id="outlined-basic"
+            size="small"
+            label="FullName*"
+            variant="outlined"
+            defaultValue={user?.fullName}
+            error={!!errors.fullName}
+            helperText={errors.fullName ? errors.fullName.message : ``}
+            className="w-full"
+            {...register("fullName", {
+              required: "FullName is required filed",
+            })}
+          />{" "}
+          <TextField
+            id="outlined-basic"
+            size="small"
+            label="Phone"
+            variant="outlined"
+            className="w-full"
+            type="number"
+            defaultValue={user?.phone}
+            error={!!errors.phone}
+            helperText={errors.phone ? errors.phone.message : ``}
+            {...register("phone", {
+              minLength: {
+                value: 10,
+                message: "Phone must be exactly 10 characters",
+              },
+              maxLength: {
+                value: 10,
+                message: "Phone must be exactly 10 characters",
+              },
+            })}
           />
-          <FormControlLabel
-            {...register("gender")}
-            value={false}
-            control={<Radio />}
-            label="Male"
-          />
-        </RadioGroup>
-      </FormControl>
-      <div className="flex justify-end">
-        <Button
+        </div>
+        {/* <TextField
+          id="outlined-basic"
+          size="small"
+          label="Email*"
           variant="outlined"
-          className="btn rounded-full normal-case text-lime-600"
-          color="success"
-          size="medium"
-        >
-          Cancel
-        </Button>
-        <Button
-          type="submit"
-          variant="contained"
-          className="btn ml-3 rounded-full normal-case"
-          size="medium"
-          color="success"
-          disableElevation
-          onClick={handleSubmit(handleSave)}
-        >
-          Save
-        </Button>
+          className="w-full"
+          defaultValue={user?.email}
+          error={!!errors.email}
+          helperText={errors.email ? errors.email.message : ``}
+          {...register("email", { required: "email is required filed" })}
+        /> */}
+        <div className="flex gap-5">
+          <TextField
+            id="outlined-basic"
+            size="small"
+            label="address"
+            variant="outlined"
+            className="w-full"
+            defaultValue={user?.address}
+            error={!!errors.address}
+            helperText={errors.address ? errors.address.message : ``}
+            {...register("address")}
+          />
+          <TextField
+            id="outlined-basic"
+            size="small"
+            label="CCCD*"
+            type="number"
+            defaultValue={user?.cccd}
+            variant="outlined"
+            className="w-full"
+            error={!!errors.cccd}
+            helperText={errors.cccd ? errors.cccd.message : ``}
+            {...register("cccd", {
+              required: "CCCD is required filed",
+              minLength: {
+                value: 12,
+                message: "CCCD must be exactly 12 characters",
+              },
+              maxLength: {
+                value: 12,
+                message: "CCCD must be exactly 12 characters",
+              },
+            })}
+          />
+        </div>
+        <div className="flex gap-5">
+          <TextField
+            error={!!errors.dateOfBirth}
+            helperText={errors.dateOfBirth ? errors.dateOfBirth.message : ``}
+            size="small"
+            label="DOB"
+            type="date"
+            defaultValue={user?.dateOfBirth}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            inputProps={{
+              max: format(today, "yyyy-MM-dd"), // Set your desired maximum date
+            }}
+            // defaultValue={
+            //   classEdit ? classEdit.timeStart : formattedTomorrow
+            // }
+            className="w-full"
+            {...register("dateOfBirth", {
+              required: "Date of birth is require",
+            })}
+          />
+        </div>
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue={!!user?.gender}
+            name="radio-buttons-group"
+            row
+          >
+            <FormControlLabel
+              {...register("gender")}
+              value={false}
+              control={<Radio />}
+              label="Female"
+            />
+            <FormControlLabel
+              {...register("gender")}
+              value={true}
+              control={<Radio />}
+              label="Male"
+            />
+          </RadioGroup>
+        </FormControl>
+        <div className="flex justify-end">
+          <Button
+            variant="outlined"
+            className="btn rounded-full normal-case text-lime-600"
+            color="success"
+            size="medium"
+          >
+            Cancel
+          </Button>
+          <Button
+            type="submit"
+            variant="contained"
+            className="btn ml-3 rounded-full normal-case"
+            size="medium"
+            color="success"
+            disableElevation
+            onClick={handleSubmit(handleSave)}
+          >
+            Save
+          </Button>
+        </div>
       </div>
-    </Box>
+    </div>
   );
 };
 
