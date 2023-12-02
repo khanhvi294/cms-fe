a1cba571dfff08364f52579695a5af0c00a87deb
Vi Nguyen <khanhvi2941@gmail.com>
2023-12-03 00:16:26 +0700

    see more student ,employee

--- src/features/admin/Employees/Employees.jsx
+++ src/features/admin/Employees/Employees.jsx
@@ -25,6 +25,7 @@ import {
   getEmployees,
   updateEmployee,
 } from "../../../services/employeeService";
+import ModalMoreInfo from "../../../components/admin/moreInfo/ModalMoreInfo";
 
 const Employees = () => {
   const {
@@ -38,6 +39,8 @@ const Employees = () => {
   const formattedToday = format(today, "yyyy-MM-dd");
   const [openDelete, setOpenDelete] = useState(false);
   const [employeeDelete, setEmployeeDelete] = useState(false);
+  const [openSeeInfo, setOpenSeeInfo] = useState(false);
+  const [itemSee, setItemSee] = useState(null);
   const columns = [
     {
       field: "id",
@@ -95,6 +98,51 @@ const Employees = () => {
       type: "actions",
       headerName: "Actions",
       getActions: (params) => [
+        <GridActionsCellItem
+          icon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              id="Eye"
+              width={15}
+              onClick={() => {
+                setOpenSeeInfo(true);
+                setItemSee(params.row);
+              }}
+            >
+              <g
+                data-name="Layer 2"
+                fill="#151515"
+                className="color000000 svgShape"
+              >
+                <g
+                  data-name="eye"
+                  fill="#151515"
+                  className="color000000 svgShape"
+                >
+                  <rect
+                    width="24"
+                    height="24"
+                    opacity="0"
+                    fill="#151515"
+                    className="color000000 svgShape"
+                  ></rect>
+                  <path
+                    d="M21.87 11.5c-.64-1.11-4.16-6.68-10.14-6.5-5.53.14-8.73 5-9.6 6.5a1 1 0 0 0 0 1c.63 1.09 4 6.5 9.89 6.5h.25c5.53-.14 8.74-5 9.6-6.5a1 1 0 0 0 0-1zM12.22 17c-4.31.1-7.12-3.59-8-5 1-1.61 3.61-4.9 7.61-5 4.29-.11 7.11 3.59 8 5-1.03 1.61-3.61 4.9-7.61 5z"
+                    fill="#151515"
+                    className="color000000 svgShape"
+                  ></path>
+                  <path
+                    d="M12 8.5a3.5 3.5 0 1 0 3.5 3.5A3.5 3.5 0 0 0 12 8.5zm0 5a1.5 1.5 0 1 1 1.5-1.5 1.5 1.5 0 0 1-1.5 1.5z"
+                    fill="#151515"
+                    className="color000000 svgShape"
+                  ></path>
+                </g>
+              </g>
+            </svg>
+          }
+          label="See"
+        />,
         <GridActionsCellItem
           icon={
             <svg
@@ -517,6 +565,11 @@ const Employees = () => {
         setOpen={setOpenDelete}
         deleteMutation={deleteEmployeeMutation}
         deleteId={employeeDelete?.id}
+      />
+      <ModalMoreInfo
+        open={openSeeInfo}
+        setOpen={setOpenSeeInfo}
+        user={itemSee}
       />
     </>
   );
--- src/features/admin/Student/Students.jsx
+++ src/features/admin/Student/Students.jsx
@@ -24,6 +24,7 @@ import {
 import { toast } from "react-toastify";
 import { format } from "date-fns";
 import ModalConfirmDelete from "../../../components/Modal/modalConfirmDelete";
+import ModalMoreInfo from "../../../components/admin/moreInfo/ModalMoreInfo";
 
 const Students = () => {
   const [open, setOpen] = useState(false);
@@ -39,6 +40,8 @@ const Students = () => {
   const [openDelete, setOpenDelete] = useState(false);
   const [studentDelete, setStudentDelete] = useState(false);
   const [rows, setRows] = useState([]);
+  const [openSeeInfo, setOpenSeeInfo] = useState(false);
+  const [itemSee, setItemSee] = useState(null);
 
   const {
     register,
@@ -89,6 +92,51 @@ const Students = () => {
       type: "actions",
       headerName: "Actions",
       getActions: (params) => [
+        <GridActionsCellItem
+          icon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              id="Eye"
+              width={15}
+              onClick={() => {
+                setOpenSeeInfo(true);
+                setItemSee(params.row);
+              }}
+            >
+              <g
+                data-name="Layer 2"
+                fill="#151515"
+                className="color000000 svgShape"
+              >
+                <g
+                  data-name="eye"
+                  fill="#151515"
+                  className="color000000 svgShape"
+                >
+                  <rect
+                    width="24"
+                    height="24"
+                    opacity="0"
+                    fill="#151515"
+                    className="color000000 svgShape"
+                  ></rect>
+                  <path
+                    d="M21.87 11.5c-.64-1.11-4.16-6.68-10.14-6.5-5.53.14-8.73 5-9.6 6.5a1 1 0 0 0 0 1c.63 1.09 4 6.5 9.89 6.5h.25c5.53-.14 8.74-5 9.6-6.5a1 1 0 0 0 0-1zM12.22 17c-4.31.1-7.12-3.59-8-5 1-1.61 3.61-4.9 7.61-5 4.29-.11 7.11 3.59 8 5-1.03 1.61-3.61 4.9-7.61 5z"
+                    fill="#151515"
+                    className="color000000 svgShape"
+                  ></path>
+                  <path
+                    d="M12 8.5a3.5 3.5 0 1 0 3.5 3.5A3.5 3.5 0 0 0 12 8.5zm0 5a1.5 1.5 0 1 1 1.5-1.5 1.5 1.5 0 0 1-1.5 1.5z"
+                    fill="#151515"
+                    className="color000000 svgShape"
+                  ></path>
+                </g>
+              </g>
+            </svg>
+          }
+          label="See"
+        />,
         <GridActionsCellItem
           icon={
             <svg
@@ -441,6 +489,11 @@ const Students = () => {
         setOpen={setOpenDelete}
         deleteMutation={deleteStudentMutation}
         deleteId={studentDelete?.id}
+      />
+      <ModalMoreInfo
+        open={openSeeInfo}
+        setOpen={setOpenSeeInfo}
+        user={itemSee}
       />
     </>
   );
