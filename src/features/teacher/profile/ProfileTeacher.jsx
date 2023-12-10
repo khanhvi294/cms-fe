e568a7b60eb17a4ce3a54200429fa1382ca1dd21
Vi Nguyen <khanhvi2941@gmail.com>
2023-12-10 21:45:24 +0700

    change password

--- src/features/teacher/profile/ProfileTeacher.jsx
+++ src/features/teacher/profile/ProfileTeacher.jsx
@@ -3,6 +3,9 @@ import ProfileUpdate from "../../../components/Profile/Profile/Profile";
 import { useMutation, useQueryClient } from "react-query";
 import { updateInfoEmployee } from "../../../services/employeeService";
 import { toast } from "react-toastify";
+import { Box, Tab, Tabs, Typography } from "@mui/material";
+import { useState } from "react";
+import ChangePassword from "../../../components/Profile/ChangePassword/ChangePassword";
 
 const ProfileTeacher = () => {
   const user = useSelector((state) => state.user.data.info);
@@ -21,16 +24,69 @@ const ProfileTeacher = () => {
       toast.error(err.message);
     },
   });
-  console.log(user?.accountEmployee, user?.role);
-  console.log(user);
+  function CustomTabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  }
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
-    <div className="w-[80%] mx-auto mt-16">
-      <ProfileUpdate
-        user={user?.accountEmployee}
-        updateInfo={updateProfileMutation}
-        role={user.role}
-      />
-    </div>
+    <>
+      <Box sx={{ width: "100%" }} className="bg-white">
+        <Box className="h-8 border-b rounded-md">
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+            className="bg-[#e7e5e5] !normal-case"
+            sx={{
+              ".Mui-selected": {
+                color: `black !important`,
+              },
+            }}
+          >
+            <Tab className=" !normal-case" label="Profile" {...a11yProps(0)} />
+            <Tab className=" !normal-case" label="Password" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        <div className="w-[80%] mx-auto mt-16">
+          <ProfileUpdate
+            user={user?.accountEmployee}
+            updateInfo={updateProfileMutation}
+            role={user.role}
+          />
+        </div>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <ChangePassword />
+      </CustomTabPanel>
+    </>
   );
 };
 
--- src/features/user/ProfileStudent/ProfileStudent.jsx
+++ src/features/user/ProfileStudent/ProfileStudent.jsx
@@ -6,6 +6,9 @@ import { setUser } from "../../../redux/slices/userSlice";
 import ProfileUpdate from "../../../components/Profile/Profile/Profile";
 import { updateProfileStudent } from "../../../services/studentService";
 import { useForm } from "react-hook-form";
+import { Box, Tab, Tabs, Typography } from "@mui/material";
+import { useState } from "react";
+import ChangePassword from "../../../components/Profile/ChangePassword/ChangePassword";
 
 const ProfileStudent = () => {
   const user = useSelector((state) => state.user.data.info);
@@ -25,15 +28,71 @@ const ProfileStudent = () => {
       toast.error(err.message);
     },
   });
-  console.log(user);
+
+  function CustomTabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  }
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div>
-      <ProfileUpdate
-        user={user?.accountStudent}
-        updateInfo={updateProfileMutation}
-        role={user?.role}
-      />
-    </div>
+    <>
+      <Box sx={{ width: "100%" }} className="bg-white">
+        <Box className="h-8 border-b rounded-md">
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+            className="bg-[#e7e5e5] !normal-case"
+            sx={{
+              ".Mui-selected": {
+                color: `black !important`,
+              },
+            }}
+          >
+            <Tab className=" !normal-case" label="Profile" {...a11yProps(0)} />
+            <Tab className=" !normal-case" label="Password" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        <div>
+          <ProfileUpdate
+            user={user?.accountStudent}
+            updateInfo={updateProfileMutation}
+            role={user?.role}
+          />
+        </div>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <ChangePassword />
+      </CustomTabPanel>
+    </>
   );
 };
 
